87879a9f6ca82c80151ef97854604eb245302568
Adeoye Adeoluwa <adeoluadeoye7@gmail.com>
2024-11-02 00:15:10 +0100

    update props validation and User Card component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./header.css";
 import CTA from "./CTA";
 import HeaderSocials from "./HeaderSocials";
+
 const Header = () => {
   return (
     <header id="home">
