bbbd019db4da20086ff703edb29ad1cf7a35900e
Adeoye Adeoluwa <adeoluadeoye7@gmail.com>
2024-11-01 11:12:06 +0100

    address slider

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
-import Testimonials from "./components/testimonials/testimonials";
+// import Testimonials from "./components/testimonials/testimonials";
 import Contact from "./components/contact/Contact";
-// import Blog from "./components/Blog/Blog";
+import Blog from "./components/Blog/Blog";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -19,9 +19,9 @@ function App() {
       <Experience />
       <Services />
       <Portfolio />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
-      {/* <Blog /> */}
+      <Blog />
       <Footer />
     </>
   );
--- src/components/Blog/BlogApi.js
+++ src/components/Blog/BlogApi.js
@@ -1,37 +1,37 @@
-// const BlogApi = [
-//     {
-//         id: 1,
-//         image: "./images/blog-01.png",
-//         date: "8th Janaury, 2024",
-//         title_one: "Mastering React: A Guide to Modern Frontend Development",
-//         desc_one: "Explore the latest advancements in React.js and elevate your frontend development skills. Dive into topics like React Hooks, Context API, and state management. This guide will empower you to build dynamic and efficient user interfaces that meet the demands of modern web development.",
-//         title_two: "Revolutionizing UX: Design Principles for Exceptional User Experiences",
-//         desc_two: "Unlock the secrets of user-centric design and create memorable experiences. Learn about UX principles, wireframing, and prototyping. Elevate your UX design game with insights into user psychology and behavior. Craft interfaces that resonate with your audience and keep them engaged.",
-//         title_three: "Inside Google: Innovations Shaping the Future of Technology",
-//         desc_three: "Delve into the groundbreaking innovations coming out of Google. From AI and machine learning to the latest in search algorithms, discover how Google continues to shape the digital landscape. Stay informed about the tech giant's projects and their impact on the world.",
-//     },
-//     {
-//         id: 2,
-//         image: "./images/blog-02.png",
-//         date: "10th January, 2024",
-//         title_one: "Meta's Vision: Navigating the Metaverse and Beyond",
-//         desc_one: "Embark on a journey into the Metaverse with Meta (formerly Facebook). Uncover Meta's vision for the future, including augmented reality, virtual reality, and the evolution of social connections. Explore the implications of the Metaverse on communication, entertainment, and business.",
-//         title_two: "Frontend Revolution: Exploring the Latest Technologies in Web Development",
-//         desc_two: "Stay ahead of the curve with insights into the newest frontend technologies. From progressive web apps (PWAs) to the latest JavaScript frameworks, this article covers the tools and trends defining the frontend landscape. Elevate your projects with cutting-edge technologies and best practices.",
-//         title_three: "Crafting Immersive Experiences: The Power of WebGL in Web Development",
-//         desc_three: "Dive into the world of WebGL and discover how it's transforming web experiences. From 3D graphics to interactive animations, explore the potential of WebGL in web development. Learn how leading websites leverage WebGL to create immersive and visually stunning user interfaces.",
-//     },
-//     {
-//         id: 3,
-//         image: "./images/blog-03.png",
-//         date: "13th December, 2023",
-//         title_one: "The Web's Future: Trends and Predictions for 2024",
-//         desc_one: "Get a sneak peek into the future of web development. Explore emerging trends, technologies, and methodologies that will shape the web landscape in 2024. Stay informed about advancements in areas like AI integration, responsive design, and the evolution of user interfaces.",
-//         title_two: "Navigating the JavaScript Framework Landscape: A Comprehensive Guide",
-//         desc_two: "Choosing the right JavaScript framework is crucial for project success. This guide compares React, Angular, and Vue.js, providing insights into their strengths and best use cases. Make informed decisions when selecting a framework for your next web development venture.",
-//         title_three: "The UX Revolution: Merging Design and Psychology for Impactful Experiences",
-//         desc_three: "Explore the intersection of design and psychology in UX. Learn how to create emotionally resonant user experiences by understanding user behavior and preferences. This article delves into the psychological principles that can elevate your UX design and leave a lasting impression.",
-//     },
-// ];
+const BlogApi = [
+    {
+        id: 1,
+        image: "./images/blog-01.png",
+        date: "8th Janaury, 2024",
+        title_one: "Mastering React: A Guide to Modern Frontend Development",
+        desc_one: "Explore the latest advancements in React.js and elevate your frontend development skills. Dive into topics like React Hooks, Context API, and state management. This guide will empower you to build dynamic and efficient user interfaces that meet the demands of modern web development.",
+        title_two: "Revolutionizing UX: Design Principles for Exceptional User Experiences",
+        desc_two: "Unlock the secrets of user-centric design and create memorable experiences. Learn about UX principles, wireframing, and prototyping. Elevate your UX design game with insights into user psychology and behavior. Craft interfaces that resonate with your audience and keep them engaged.",
+        title_three: "Inside Google: Innovations Shaping the Future of Technology",
+        desc_three: "Delve into the groundbreaking innovations coming out of Google. From AI and machine learning to the latest in search algorithms, discover how Google continues to shape the digital landscape. Stay informed about the tech giant's projects and their impact on the world.",
+    },
+    {
+        id: 2,
+        image: "./images/blog-02.png",
+        date: "10th January, 2024",
+        title_one: "Meta's Vision: Navigating the Metaverse and Beyond",
+        desc_one: "Embark on a journey into the Metaverse with Meta (formerly Facebook). Uncover Meta's vision for the future, including augmented reality, virtual reality, and the evolution of social connections. Explore the implications of the Metaverse on communication, entertainment, and business.",
+        title_two: "Frontend Revolution: Exploring the Latest Technologies in Web Development",
+        desc_two: "Stay ahead of the curve with insights into the newest frontend technologies. From progressive web apps (PWAs) to the latest JavaScript frameworks, this article covers the tools and trends defining the frontend landscape. Elevate your projects with cutting-edge technologies and best practices.",
+        title_three: "Crafting Immersive Experiences: The Power of WebGL in Web Development",
+        desc_three: "Dive into the world of WebGL and discover how it's transforming web experiences. From 3D graphics to interactive animations, explore the potential of WebGL in web development. Learn how leading websites leverage WebGL to create immersive and visually stunning user interfaces.",
+    },
+    {
+        id: 3,
+        image: "./images/blog-03.png",
+        date: "13th December, 2023",
+        title_one: "The Web's Future: Trends and Predictions for 2024",
+        desc_one: "Get a sneak peek into the future of web development. Explore emerging trends, technologies, and methodologies that will shape the web landscape in 2024. Stay informed about advancements in areas like AI integration, responsive design, and the evolution of user interfaces.",
+        title_two: "Navigating the JavaScript Framework Landscape: A Comprehensive Guide",
+        desc_two: "Choosing the right JavaScript framework is crucial for project success. This guide compares React, Angular, and Vue.js, providing insights into their strengths and best use cases. Make informed decisions when selecting a framework for your next web development venture.",
+        title_three: "The UX Revolution: Merging Design and Psychology for Impactful Experiences",
+        desc_three: "Explore the intersection of design and psychology in UX. Learn how to create emotionally resonant user experiences by understanding user behavior and preferences. This article delves into the psychological principles that can elevate your UX design and leave a lasting impression.",
+    },
+];
 
-// export default BlogApi;
+export default BlogApi;
