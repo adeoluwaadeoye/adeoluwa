ba617a5ff32072963659927bfc904135180c04db
Adeoluwa Adeoye <adeoluadeoye7@gmail.com>
2025-06-19 10:24:08 +0100

    Update SEO

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,157 +1,58 @@
 import React from "react";
-import "./experience.css";
 import { BsPatchCheckFill } from "react-icons/bs";
+import "./experience.css";
 
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>What Skills I Have</h5>
-      <h2>My Experience</h2>
+      <h5>Skills That Power My Work</h5>
+      <h2>Experience</h2>
 
-      <div className="container experience__container">
-        <div className="experience__frontend">
-          <h3>Development & Design</h3>
-          <div className="experience__content">
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>JavaScript</h4>
-              </div>
-            </div>
-
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>Bootstrap</h4>
-              </div>
-            </div>
-
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>Figma</h4>
-              </div>
-            </div>
-
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>Tailwind</h4>
-              </div>
-            </div>
-
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>Git & Github</h4>
-              </div>
-            </div>
-
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>React</h4>
-              </div>
-            </div>
-
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>TypeScript</h4>
-              </div>
-            </div>
-
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>Next.js</h4>
-              </div>
-            </div>
-
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>Framer Motion</h4>
-              </div>
-            </div>
-
-            <div className="experience-details">
-              <BsPatchCheckFill className="details__icon" />
-              <div>
-                <h4>Styled Components</h4>
-              </div>
-            </div>
-          </div>
+      <div className="experience__wrapper container">
+        {/* Technical Skills */}
+        <div className="experience__category">
+          <h3>Development & Tools</h3>
+          <ul className="skills__list">
+            {[
+              "JavaScript",
+              "React",
+              "Next.js",
+              "TypeScript",
+              "Tailwind CSS",
+              "Framer Motion",
+              "Styled Components",
+              "Git & GitHub",
+              "Figma",
+            ].map((skill) => (
+              <li className="skill__item" key={skill}>
+                <BsPatchCheckFill className="skill__icon" />
+                <span>{skill}</span>
+              </li>
+            ))}
+          </ul>
         </div>
 
-        <div className="experience__backend">
-          <div className="experience__uxdesign">
-            <h3>Soft Skills</h3>
-            <div className="experience__content">
-              <div className="experience-details">
-                <BsPatchCheckFill className="details__icon" />
-                <div>
-                  <h4>Collaboration</h4>
-                </div>
-              </div>
-
-              <div className="experience-details">
-                <BsPatchCheckFill className="details__icon" />
-                <div>
-                  <h4>Problem-Solving</h4>
-                </div>
-              </div>
-
-              <div className="experience-details">
-                <BsPatchCheckFill className="details__icon" />
-                <div>
-                  <h4>Communication</h4>
-                </div>
-              </div>
-
-              <div className="experience-details">
-                <BsPatchCheckFill className="details__icon" />
-                <div>
-                  <h4>Adaptability</h4>
-                </div>
-              </div>
-
-              <div className="experience-details">
-                <BsPatchCheckFill className="details__icon" />
-                <div>
-                  <h4>Flexibility</h4>
-                </div>
-              </div>
-
-              <div className="experience-details">
-                <BsPatchCheckFill className="details__icon" />
-                <div>
-                  <h4>Time Management</h4>
-                </div>
-              </div>
-
-              <div className="experience-details">
-                <BsPatchCheckFill className="details__icon" />
-                <div>
-                  <h4>Creativity</h4>
-                </div>
-              </div>
-
-              <div className="experience-details">
-                <BsPatchCheckFill className="details__icon" />
-                <div>
-                  <h4>Attention to Detail</h4>
-                </div>
-              </div>
-
-              <div className="experience-details">
-                <BsPatchCheckFill className="details__icon" />
-                <div>
-                  <h4>Critical Thinking</h4>
-                </div>
-              </div>
-            </div>
-          </div>
+        {/* Soft Skills */}
+        <div className="experience__category">
+          <h3>Soft Skills</h3>
+          <ul className="skills__list">
+            {[
+              "Communication",
+              "Problem-Solving",
+              "Creativity",
+              "Critical Thinking",
+              "Collaboration",
+              "Adaptability",
+              "Time Management",
+              "Attention to Detail",
+              "Growth Mindset",
+            ].map((skill) => (
+              <li className="skill__item" key={skill}>
+                <BsPatchCheckFill className="skill__icon" />
+                <span>{skill}</span>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </section>
@@ -159,3 +60,4 @@ const Experience = () => {
 };
 
 export default Experience;
+
