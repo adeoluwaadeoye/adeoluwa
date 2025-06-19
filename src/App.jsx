d22f1f3d4b9b9ad7694a66052c48457a90d44096
Adeoluwa Adeoye <adeoluadeoye7@gmail.com>
2025-06-19 09:39:41 +0100

    Update Portolio content

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
-import Experience from "./components/experience/Experience";
+// import Experience from "./components/experience/Experience";
 // import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/testimonials";
@@ -16,7 +16,7 @@ function App() {
       <Header />
       <Nav />
       <About />
-      <Experience />
+      {/* <Experience /> */}
       {/* <Services /> */}
       <Portfolio />
       <Testimonials />
