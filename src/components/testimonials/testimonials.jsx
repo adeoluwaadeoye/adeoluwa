377d54b69a59e7791ad4358d8015ed1dab2905e3
Adeoluwa Adeoye <adeoluadeoye7@gmail.com>
2025-07-01 09:16:51 +0100

    Update in Testimonials section

--- src/components/testimonials/testimonials.jsx
+++ src/components/testimonials/testimonials.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import "./testimonials.css";
 
-// import avatar1 from "../../assets/avatar1.jpg";
+import avatar1 from "../../assets/avatar1.png";
 import avatar2 from "../../assets/avatar2.jpg";
 import avatar3 from "../../assets/avatar3.jpg";
-// import avatar4 from "../../assets/avatar4.jpg";
-import avatar5 from "../../assets/avatar5.jpg";
+
 
 import {Swiper, SwiperSlide} from "swiper/react";
 
@@ -17,24 +16,24 @@ import "swiper/css/pagination";
 import {Pagination, Autoplay, Keyboard} from "swiper/modules";
 
 const data = [
-  // {
-  //   id: 1,
-  //   avatar: avatar1,
-  //   name: "Lilian James",
-  //   review:
-  //     "I had an amazing experience with his services. He was professional, courteous, and delivered outstanding results. I highly recommend him to anyone looking for top-notch service!",
-  // },
+  {
+    id: 1,
+    avatar: avatar1,
+    name: "Koiki Yusuf Adesanya",
+    review:
+      "I had an amazing experience with his services. He was professional, courteous, and delivered outstanding results. I highly recommend him to anyone looking for top-notch service!",
+  },
   {
     id: 2,
     avatar: avatar2,
-    name: "Adeshina James",
+    name: "Matiluko James",
     review:
       "Working with Adeoluwa's team was an excellent experience! He exceeded my expectations. The attention to detail and dedication to delivering quality work were impressive. I look forward to collaborating with them again in the future.",
   },
   {
     id: 3,
     avatar: avatar3,
-    name: "Destiny Okafor",
+    name: "Joy Oke",
     review:
       "I'm extremely satisfied with the services provided by this team. Their professionalism and efficiency stood out throughout the project. I will definitely choose them for my future projects and recommend them to others seeking reliable and high-quality solutions.",
   },
@@ -45,13 +44,13 @@ const data = [
   //   review:
   //     "Adeoluwa’s expertise truly made a difference in our project. His insights and recommendations helped us streamline our process, and we achieved fantastic results. I would highly recommend his services to anyone!",
   // },
-  {
-    id: 5,
-    avatar: avatar5,
-    name: "Tobiloba Johnson",
-    review:
-      "Fantastic experience from start to finish! His dedication and creativity brought our vision to life in ways we hadn’t imagined. Adeoluwa has an eye for detail and a passion for quality. Highly recommend!",
-  },
+  // {
+  //   id: 5,
+  //   avatar: avatar5,
+  //   name: "Tobiloba Johnson",
+  //   review:
+  //     "Fantastic experience from start to finish! His dedication and creativity brought our vision to life in ways we hadn’t imagined. Adeoluwa has an eye for detail and a passion for quality. Highly recommend!",
+  // },
 ];
 
 const Testimonials = () => {
